62d8ffcc10b01a33a3b785697484f3772e2df970
beth-dixxon <bethdixondesign@gmail.com>
2022-09-21 13:21:37 +0100

    changing id/names in code

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const radioButtonRedirect = require('radio-button-redirect')
+router.use(radioButtonRedirect)
 
 // Add your routes here - above the module.exports line
 
@@ -94,5 +96,8 @@ router.get('/scores', function (req, res) {
 
 
 // Add your routes here - above the module.exports line
+
+
+
 
 module.exports = router
