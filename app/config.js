a40e651fb3819e47ea05d8f7f6d3348212a95fa8
beth-dixxon <bethdixondesign@gmail.com>
2022-12-06 11:44:34 +0000

    Changed name of service at top

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Apply for an Approved Premises (AP) placement',
+  serviceName: 'Approved Premises',
 
   // Default port that prototype runs on
   port: '3000',
