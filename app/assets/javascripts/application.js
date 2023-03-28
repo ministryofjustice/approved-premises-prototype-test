69421622cd3c4b5e0a2352071c53862851777a77
beth-dixxon <bethdixondesign@gmail.com>
2023-03-28 14:15:26 +0100

    Adding manage prototype links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,3 +12,10 @@ $(document).ready(function () {
 window.MOJFrontend.initAll()
 
 
+new MOJFrontend.ButtonMenu({
+  container: $(".moj-button-menu"),
+  mq: "(min-width: 200em)",
+  buttonText: "Actions",
+  menuClasses: "moj-button-menu__wrapper--right",
+});
+
